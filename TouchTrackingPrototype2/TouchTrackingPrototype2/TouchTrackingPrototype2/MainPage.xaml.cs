dee2422b0b9caaabc4206b4bbf95ca1fbcd8875e
Michael Herman (Trusted Digital Web) <mwherman@parallelspace.net>
2022-03-15 19:37:30 -0600

    Update 2

---
+++ TouchTrackingPrototype2/TouchTrackingPrototype2/TouchTrackingPrototype2/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using System.Windows.Input;
+
+namespace TouchTrackingPrototype2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void DoneButton_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new DonePage());
+        }
+    }
+}
